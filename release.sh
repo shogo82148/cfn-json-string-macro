2ae059598e71a124216fe3fd7bdcd08fd93b87b2
Ichinose Shogo <shogo82148@gmail.com>
2021-03-15 17:19:51 +0900

    bump up to v0.0.1

--- release.sh
+++ release.sh
@@ -22,25 +22,32 @@ cd "$DIST"
 while read -r REGION; do
     BUCKET=shogo82148-cloudformation-template-$REGION
     aws cloudformation package \
+        --region "$REGION" \
         --template-file "$DIST/template.yaml" \
         --output-template-file "$DIST/$REGION.yaml" \
         --s3-bucket "$BUCKET" \
         --s3-prefix cfn-json-string-macro/resource
-    aws s3 cp "$DIST/$REGION.yaml" "s3://$BUCKET/cfn-json-string-macro/latest.yaml"
-    aws s3 cp "$DIST/$REGION.yaml" "s3://$BUCKET/cfn-json-string-macro/v$MAJOR.$MINOR.$PATCH.yaml"
-    aws s3 cp "$DIST/$REGION.yaml" "s3://$BUCKET/cfn-json-string-macro/v$MAJOR.$MINOR.yaml"
-    aws s3 cp "$DIST/$REGION.yaml" "s3://$BUCKET/cfn-json-string-macro/v$MAJOR.yaml"
+    aws s3 cp --region "$REGION" "$DIST/$REGION.yaml" "s3://$BUCKET/cfn-json-string-macro/latest.yaml"
+    aws s3 cp --region "$REGION" "$DIST/$REGION.yaml" "s3://$BUCKET/cfn-json-string-macro/v$MAJOR.$MINOR.$PATCH.yaml"
+    aws s3 cp --region "$REGION" "$DIST/$REGION.yaml" "s3://$BUCKET/cfn-json-string-macro/v$MAJOR.$MINOR.yaml"
+    aws s3 cp --region "$REGION" "$DIST/$REGION.yaml" "s3://$BUCKET/cfn-json-string-macro/v$MAJOR.yaml"
 done << EOS
+af-south-1
+ap-east-1
 ap-northeast-1
 ap-northeast-2
+ap-northeast-3
 ap-south-1
 ap-southeast-1
 ap-southeast-2
 ca-central-1
 eu-central-1
+eu-north-1
+eu-south-1
 eu-west-1
 eu-west-2
 eu-west-3
+me-south-1
 sa-east-1
 us-east-1
 us-east-2
